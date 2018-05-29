452a888e9712969dc026711b2912bbed87fd828d
wandersonsantos01 <wanderson.santos01@gmail.com>
2018-05-29 15:53:45 -0300

    deleta e cria link

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Teste commit II</title>
+</head>
+<body>
+	<h1>
+		Alteração do arquivo pela 3ª X
+	</h1>
+
+<span style="font-size: 44px">
+	TESTE CENTOS 7 COM PHP 7.2
+</span>
+
+blablabla
+
+<br />
+<br />
+<br />
+
+<?php echo date('Y-m-d'); ?>
+
+<a href="menu1.php">
+	MENU 1
+</a>
+
+</body>
+</html>
---
+++ menu1.php
@@ -0,0 +1,5 @@
+<?php
+
+echo date('H:m:i');
+
+?>
